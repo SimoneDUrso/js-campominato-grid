2cdab87c91c196b2f52246ef6cdd5b90c109df63
SimoneDUrso <simone.durso98@gmail.com>
2024-07-05 16:09:04 +0200

    [UPD] Variable created whose value is the creation of the div

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 // Funzione per la creazione dei quadrati nella griglia
 function createSquare() {
+    // currentElement contiene come valore il DIV creato
+    let currentElement = document.createElement('div');
 
 }
