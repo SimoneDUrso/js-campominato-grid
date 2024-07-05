5bf21710e4e3bc31fa2bf124c8853c22ecca0e41
SimoneDUrso <simone.durso98@gmail.com>
2024-07-05 16:28:20 +0200

    [UPD] I recall the function

--- js/script.js
+++ js/script.js
@@ -24,6 +24,9 @@ btn.addEventListener("click", function () {
     // Ciclo FOR per creare una griglia 10x10
     for (i = 0; i < 100; i++) {
 
+        // richiamo la funzione per la creazione del quadrato
+        let currentSquare = createSquare();
+
 
 
     }
